ee9bffe0699f9436b64890e0dd5209da76cf8f73
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-15 15:06:47 -0700

    First commit; added solution using a loop

---
+++ src/main.py
@@ -0,0 +1,62 @@
+'''
+Created on Sep 14, 2017
+
+@author: jlearx
+'''
+
+def RemoveDupsLoop(oldlist):
+    newlist = []
+    [newlist.append(e) for e in oldlist if (newlist.count(e) == 0)]
+    return newlist
+
+def RemoveDupsSet(oldlist):
+    newlist = []
+    
+    return newlist
+
+def GetList():
+    numlist = []
+    
+    while (len(numlist) == 0):
+        listIn = input("Please enter a list of numbers inside []: ")
+        start = listIn.find("[")
+        finish = listIn.find("]")
+        
+        if ((start >= 0) and (finish > 0) and (finish != start + 1)):
+            listIn = listIn[start + 1:finish]
+            listIn = listIn.replace(" ", "")
+            listStr = listIn.split(",")
+            numlist = listStr
+            break
+        
+    return numlist
+
+if __name__ == '__main__':
+    oldlist = GetList()
+    
+    # Use a loop to remove duplicates
+    newlist = RemoveDupsLoop(oldlist)
+    length = len(newlist)
+    
+    print("Here is the list w/o duplicates using a loop:")
+    
+    for i in range(0, length):
+        if (i < length - 1):
+            print(newlist[i], end=', ')
+        else:
+            print(newlist[i])
+    
+    newlist.clear()
+    
+    # Use a set to remove duplicates
+    newlist = RemoveDupsSet(oldlist)
+    length = len(newlist)
+    
+    print("Here is the list w/o duplicates using sets:")
+    
+    for i in range(0, length):
+        if (i < length - 1):
+            print(newlist[i], end=', ')
+        else:
+            print(newlist[i])
+            
